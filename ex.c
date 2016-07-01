1d43d39f6669b28d5cc618bd27f83e9b23fc340b
Ryan McCullagh <me@ryanmccullagh.com>
2016-07-01 17:48:11 -0500

    new changes to ex

--- ex.c
+++ ex.c
@@ -6,14 +6,20 @@
 #define one_is_double(left, right) \
 	(O_TYPE(left) == IS_DOUBLE || O_TYPE(right) == IS_DOUBLE)
 
+#define EXTRACT_KNOWN_NUMERIC_AS_DOUBLE(x) \
+	(O_TYPE((x)) == IS_DOUBLE ? O_DVAL((x)) : (double)O_LVAL((x)))
+
 Object *object_concat_helper(Object *left, Object *right) {
 	Object *retval;
 	
 	if(is_numeric(left) && is_numeric(right)) {	
 		if(is_one_double(left, right)) {
-			double result;
+			double op1, op2;
+			op1 = EXTRACT_KNOWN_NUMERIC_AS_DOUBLE(left);
+			op2 = EXTRACT_KNOWN_NUMERIC_AS_DOUBLE(right);
+			retval = newDouble(op1 + op2);
 		}	else {
-			long result;
+			retval = newLong(O_LVAL(left) + O_LVAL(right));	
 		}	
 	} else {
 		// CONVERT EVERYTHING TO STRING AND stringCat
